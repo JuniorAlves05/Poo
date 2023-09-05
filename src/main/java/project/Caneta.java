497c10542c82adc4cc54bcda7132e52e6b7fcbc1
JuniorAlves05 <junioralveswork01@gmail.com>
2023-09-04 21:43:47 -0300

    Metodos Getter, Setter e Construtor

--- src/main/java/project/Caneta.java
+++ src/main/java/project/Caneta.java
@@ -1,33 +1,45 @@
 package project;
 
 public class Caneta {
-
     public String modelo;
-    public String cor;
     private float ponta;
-    protected int carga;
+    private String cor;
     private boolean tampada;
 
-    public void status (){
-        System.out.println("Modelo :" + this.modelo);
-        System.out.println("Uma caneta : " + this.cor);
-        System.out.println("Ponta :" + this.ponta);
-        System.out.println("Carga :" + this.carga);
-        System.out.println("Esta tampada ? " + this.tampada);
+    public Caneta(String m, String c, float p){
+        this.modelo=m;
+        this.cor = c ;
+        this.ponta = p;
+        this.tampar();
+
     }
 
-
-    public void rabiscar (){
-        if(this.tampada == true){
-            System.out.println("Erro ! Nao posso rabiscar");
-        } else {
-            System.out.println("Estou rabiscando");
-        }
+    public String getModelo(){
+        return this.modelo;
     }
+    public void setModelo(String m){
+        this.modelo =m;
+    }
+    public float getPonta(){
+        return this.ponta;
+    }
+    public void setPonta(float p){
+        this.ponta = p ;
+    }
+
     public void tampar (){
         this.tampada = true;
     }
+
     public void destampar (){
         this.tampada = false;
     }
+    public void status (){
+        System.out.println(" Sobre A Caneta :");
+        System.out.println(" Modelo : "+ this.modelo);
+        System.out.println(" Ponta : " + this.ponta);
+        System.out.println(" Cor : " + this.cor);
+        System.out.println(" Tampada : " + this.tampada);
+    }
+
 }
--- src/main/java/project/projeto.java
+++ src/main/java/project/projeto.java
@@ -2,15 +2,10 @@ package project;
 
 public class projeto {
     public static void main(String[] args) {
-        Caneta c1 = new Caneta();
-        c1.modelo ="Bic Cristal";
-        c1.cor= "Azul";
-        //c1.ponta = 0.5f;
-        c1.carga= 80;
-       // c1.tampada = false;
-        c1.destampar();
-
+        Caneta c1 =new Caneta("Nic","Amarelo",0.4f);
         c1.status();
-        c1.rabiscar();
+        System.out.println();
+        Caneta c2 = new Caneta("Bic","Laranja",0.5f);
+        c2.status();
     }
 }
