019e31647688914da164c323cd4384390a600a0a
JuniorAlves05 <junioralveswork01@gmail.com>
2023-09-05 22:37:29 -0300

    Modificacao do projeto para ContaBanco

--- src/main/java/project/projeto.java
+++ src/main/java/project/projeto.java
@@ -2,10 +2,22 @@ package project;
 
 public class projeto {
     public static void main(String[] args) {
-        Caneta c1 =new Caneta("Nic","Amarelo",0.4f);
-        c1.status();
-        System.out.println();
-        Caneta c2 = new Caneta("Bic","Laranja",0.5f);
-        c2.status();
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumConta(1111);
+        p1.setDono("Jubileu");
+        p1.abrirConta("CC");
+
+        ContaBanco p2 = new ContaBanco();
+        p2.setNumConta(2222);
+        p2.setDono("Creuza");
+        p2.abrirConta("CP");
+
+        p1.depositar(100);
+        p2.depositar(500);
+        p2.sacar(100);
+
+        p1.estadoAtual();
+        p2.estadoAtual();
+
     }
 }
